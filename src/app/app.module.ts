38cc0c87f17f622925018533db1d074e29b5dee3
Jonathan Hogate <jonathan.hogate@jbhunt.com>
2021-08-23 10:00:00 -0500

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ChildComponent } from './child/child.component';
+
+@NgModule({
+  imports: [
+    BrowserModule
+  ],
+  declarations: [
+    AppComponent,
+    ChildComponent,
+    HeaderComponent
+  ],
+  bootstrap: [
+    AppComponent
+  ]
+})
+
+export class AppModule {  }
