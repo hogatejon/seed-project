bc7b7b5f0b74818c3e6e1e33db9ca02c3e24f65c
Jonathan Hogate <jonathan.hogate@jbhunt.com>
2021-08-24 09:06:13 -0500

    simple service example

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HeaderTitleService } from '../services/header-title.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  userName: string = 'Jonathan';
+  welcomeMessage: string;
+
+  constructor(private readonly headerTitleService: HeaderTitleService) { }
 
   ngOnInit(): void {
+    this.welcomeMessage = this.headerTitleService.getTitle();
   }
 
 }
